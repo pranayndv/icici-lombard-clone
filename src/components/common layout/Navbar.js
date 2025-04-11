f01779e08f58664a20a6e47ec4813c156ef94911
pranayndv <142014316+pranayndv@users.noreply.github.com>
2025-04-11 18:09:47 +0530

    fix

--- src/components/common layout/Navbar.js
+++ src/components/common layout/Navbar.js
@@ -14,7 +14,7 @@ const Navbar = () => {
     <div className='relative'>
       <div className="top-navbar  bg-[#F8F6F6] text-[13px] flex md:justify-between items-center md:px-10 py-1">
         <div className="left py-1 flex md:space-x-4 space-x-2 text-[13px]">
-          <div className='flex items-center px-3 space-x-3'>
+          <div className='flex items-center md:px-3 px-1 space-x-3'>
           <svg width="30" height="18" viewBox="0 0 30 18" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path fill-rule="evenodd" clip-rule="evenodd" d="M14.9984 0.962891C10.5665 0.962891 6.96094 4.56847 6.96094 9.00032C6.96094 13.4322 10.5665 17.0378 14.9984 17.0378C19.4302 17.0378 23.0358 13.4322 23.0358 9.00032C23.0358 4.56847 19.4302 0.962891 14.9984 0.962891ZM15 18C10.0374 18 6 13.9626 6 9C6 4.03739 10.0374 0 15 0C19.9626 0 24 4.03739 24 9C24 13.9626 19.9626 18 15 18Z" fill="#282828"/>
           <path fill-rule="evenodd" clip-rule="evenodd" d="M12.9433 3.37332C12.9433 3.37332 13.8691 4.54958 14.2088 5.79142C14.4017 6.62143 13.8514 6.80588 13.4928 6.94373C13.1342 7.08157 12.526 7.52917 13.0559 8.7322C13.5859 9.93523 14.6442 11.0705 15.2141 11.2674C15.784 11.4643 15.9134 11.6158 16.7369 10.9086C17.3239 10.4045 18.2511 11.1205 18.7354 11.5735C18.9306 11.756 19.8576 12.7932 19.8576 12.7932C19.8576 12.7932 20.4806 13.7232 19.1207 14.4967C17.7609 15.2702 17.3709 15.1773 15.9051 14.0426C14.4393 12.9079 11.2742 10.5088 10.0318 5.32069C9.95923 5.01364 10.022 4.72275 10.1081 4.45152C10.2777 3.9167 11.3022 3.11491 11.8461 3.02319C12.2299 2.95843 12.6392 3.01535 12.9433 3.37332Z" fill="#282828"/>
@@ -24,10 +24,10 @@ const Navbar = () => {
 
             <a href="Tel:18002666">1800 2666</a>
           </div>|
-          <div className='flex items-center px-3 space-x-3 text-[13px]'>
+          <div className='flex items-center md:px-3 px-1 space-x-3 text-[13px]'>
             <a href="Tel:18002666">Call Back</a>
           </div>|
-          <div className='px-3'>
+          <div className='md:px-3 px-1'>
             <span className='bg-white rounded-3xl h-[23px] flex space-x-2 justify-center items-center px-2 text-[13px]'>
               <span className='bg-[#26BA40] rounded-full w-3 h-3'></span>
               <p>Live Chat</p>
@@ -37,7 +37,7 @@ const Navbar = () => {
 
         
         <button
-            className="flex items-center focus:outline-none"
+            className="flex items-center px-1"
             onClick={() => setIsOpen(!isOpen)}
           >
             <svg
