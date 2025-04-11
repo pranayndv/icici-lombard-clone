79b8a88cade36bb420b05b68c7a6432a39c4ca11
pranayndv <142014316+pranayndv@users.noreply.github.com>
2025-04-11 18:16:37 +0530

    clear

--- src/components/common layout/Navbar.js
+++ src/components/common layout/Navbar.js
@@ -37,7 +37,7 @@ const Navbar = () => {
 
         
         <button
-            className="flex items-center px-1"
+            className="flex items-center px-1 md:hidden"
             onClick={() => setIsOpen(!isOpen)}
           >
             <svg
